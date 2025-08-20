11a3ed8637c07d3518462da9e2979e71006b5daa
Aalok <mandalavp1277@gmail.com>
2025-08-20 18:29:29 +0545

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>  
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3696743684086427"
+     crossorigin="anonymous"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
